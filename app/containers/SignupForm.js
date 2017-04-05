36084d1fc09f90fc01a6df796cfb69c902538de9
denisflorkin <denisflorkin@gmail.com>
2017-04-05 08:03:47 +0200

    connect dispatch in signupform + set up updateFormState action roundtrip

--- app/containers/SignupForm.js
+++ app/containers/SignupForm.js
@@ -2,35 +2,74 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import {
+  updateFormState,
+  postForm,
+} from './actions'
 import { SIGNUP_FORM_ID } from './constants'
+
+import { getFormData } from '../utils/getFormData'
 
 import Input from '../components/Input'
 import FeedBackMsg from '../components/FeedBackMsg'
 
 
-export function SignupForm(props) {
-  const { stuff } = props
+export class SignupForm extends React.PureComponent {
+  handleFormSubmit(e) {
+    e.preventDefault()
+    const theForm = document.forms[SIGNUP_FORM_ID]
+    console.log(getFormData(theForm))
+  }
 
-  return (
-    <form name={SIGNUP_FORM_ID} >
+  handleFormChange(e) {
+    const theForm = document.forms[SIGNUP_FORM_ID]
+    const { onFormChange } = this.props
+    onFormChange(getFormData(theForm))
+    console.log(getFormData(theForm))
+  }
 
-      <label htmlFor="name">Name</label>
-      <Input id="name" name="name" type="text" />
+  render() {
+    const {
+      props,
 
-      <label htmlFor="email">Email</label>
-      <Input id="email" name="email" type="email" />
+      handleFormSubmit,
+      handleFormChange,
+    } = this
 
-      <label htmlFor="password">Password</label>
-      <Input id="password" name="password" type="password" />
+    return (
+      <form name={SIGNUP_FORM_ID} onSubmit={handleFormSubmit.bind(this)} >
 
-      <label htmlFor="password-confirm">Password confirmation</label>
-      <Input id="password-confirm"
-        name="password-confirm" type="password" />
+        <label htmlFor="name">Name</label>
+        <Input id="name" name="name" type="text"
+          onChange={handleFormChange.bind(this)} />
 
-      <FeedBackMsg />
-      <Input type='submit' value="Send"/>
-    </form>
-  )
+        <label htmlFor="email">Email</label>
+        <Input id="email" name="email" type="email"
+          onChange={handleFormChange.bind(this)} />
+
+        <label htmlFor="password">Password</label>
+        <Input id="password" name="password" type="password"
+          onChange={handleFormChange.bind(this)} />
+
+        <label htmlFor="password-confirm">Password confirmation</label>
+        <Input id="password-confirm" name="password-confirm" type="password"
+          onChange={handleFormChange.bind(this)} />
+
+        <FeedBackMsg />
+        <Input type='submit' value="Send" />
+      </form>
+    )
+  }
 }
 
-export default SignupForm
+function mapDispatchToProps(dispatch) {
+  return {
+    onFormChange: (payload) => dispatch(updateFormState(payload)),
+    onPostForm: (payload) => dispatch(postForm(payload)),
+  }
+}
+
+
+// export default SignupForm
+export default connect(null, mapDispatchToProps)(SignupForm)
+// connect([mapStateToProps], [mapDispatchToProps], [mergeProps], [options])
---
+++ app/utils/getFormData.js
@@ -0,0 +1,48 @@
+/**
+  * getFormData
+  * give it a HTMLFormElement, it'll give you back
+  * a nice key:value object with your form fieldName:value
+  */
+export const isHTMLFormElement = (elem) =>
+  !!Object.prototype.toString.call(elem).match(/\[object HTMLFormElement\]/) // eslint-disable-line no-useless-escape
+
+export const isHTMLSelectElement = (elem) =>
+  !!Object.prototype.toString.call(elem).match(/\[object HTMLSelectElement\]/)  // eslint-disable-line no-useless-escape
+
+export const getFormData = (HTMLFormElement) => {
+  if (!HTMLFormElement && !isHTMLFormElement(HTMLFormElement)) {
+    return console.warn(  // eslint-disable-line no-console
+      'you must provide a valid form element reference! this is not valid :',
+      HTMLFormElement,
+      'Also do not forget to give names to your form and inputs')
+  }
+
+  const { elements } = HTMLFormElement
+  let forRes = {} // eslint-disable-line prefer-const
+  for (let i = 0; i < elements.length; i++) { // eslint-disable-line no-plusplus
+    let theName
+    let theValue
+    if (isHTMLSelectElement(elements[i])) {  // check `selected` options
+      const select = elements[i]
+      const { options } = select
+      const selectedOptions = [] // the required task
+      let j = 0
+      while (j < select.length - 1) { // go throught options:
+        if (options[j].selected) {
+          selectedOptions.push(options[j].value)
+        }
+        j += 1
+      }
+      theName = select.name
+      theValue = selectedOptions
+    } else {
+      theName = elements[i].name
+      theValue = elements[i].value
+    }
+
+    if (theValue && theName !== '') {  // ignore numerical HTMLCollectionFromControl key
+      forRes[theName] = theValue
+    }
+  }
+  return forRes
+}
