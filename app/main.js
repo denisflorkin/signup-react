c15ad23a8dd7a474db57da3e617558c1fb80457d
denisflorkin <denisflorkin@gmail.com>
2017-04-11 20:09:06 +0200

    clean main.js

--- app/main.js
+++ app/main.js
@@ -11,28 +11,7 @@ import App from './components/App'
 import signupReducer from './containers/reducer'
 
 
-
-/**
- * create the store
- */
-// If Redux DevTools Extension is installed use it, otherwise use Redux compose
-/*
-const composeEnhancers =
-  process.env.NODE_ENV !== 'production' &&
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
-
-const middlewares = [
-  thunk,
-];
-
-const enhancers = [
-  applyMiddleware(...middlewares),
-];
-*/
 const store = createTheStore(signupReducer)
-
 
 function init() {
   const appRootNode = document.getElementById('react-root')
