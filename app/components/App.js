6a354261a9fdc2eea141efba4731730fcf04c401
denisflorkin <denisflorkin@gmail.com>
2017-05-26 22:07:15 +0200

    save working copy

--- app/components/App.js
+++ app/components/App.js
@@ -1,30 +1,33 @@
 
 import React from 'react'
+import styled from 'styled-components'
 
 import theme from '../utils/theme'
 import Signupfom from '../containers/SignupForm'
 import H1 from './H1'
 
-const style = {
-  maxWidth: '380px',
-  margin: '0 auto',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  fontFamily: 'Helvetica, Arial, sans-serif',
-  padding: '16px',
-  border: '1px solid rgba(35, 70, 100, .09)',
-  boxShadow: '0 0 16px 2px rgba(35, 70, 100, .06)',
-  color: theme.darkGrey,
-  overflow: 'hidden',
-}
+
+
+const Wrapper = styled.div`
+  max-width: 380px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font-family: Helvetica, Arial, sans-serif;
+  padding: 16px;
+  border: 1px solid rgba(35, 70, 100, .09);
+  boxShadow: 0 0 16px 2px rgba(35, 70, 100, .06);
+  color: ${theme.darkGray};
+  overflow: hidden;
+`
 
 export function App() {
   return (
-    <div style={ style } >
+    <Wrapper>
       <H1>Sign Up</H1>
       <Signupfom />
-    </div>
+    </Wrapper>
   )
 }
 export default App
--- app/components/Input.js
+++ app/components/Input.js
@@ -1,9 +1,35 @@
 'use strict';
 
 import React, { PropTypes } from 'react'
+import styled from 'styled-components'
+
 import theme from '../utils/theme'
 
-const { green, red, gray } = theme
+
+const { green, red, gray, darkGray } = theme
+
+// const getBorderColor = ({ error }) => !error ?
+
+const getBorderBottomColor = ({ error }) => !error ?
+  `green}`
+  : `2px solid ${red}`
+
+
+const StyledInput = styled.input`
+  width: 100%;
+  display: block;
+  box-sizing: border-box;
+  border: 1px solid transparent;
+  border-radius: 0;
+  outline: none;
+  padding: 4px 0;
+  margin: 2px auto 16px auto;
+  font-size: 1.2em;
+  border-bottom: 2px solid ${gray};
+  /* dynmaic styles */
+  border: 2px solid ${({error}) => !error ? 'transparent' : red};
+  border-bottom: 2px solid ${({error}) => !error ? gray : red};
+`
 
 const style = {
   width: '100%',
@@ -50,8 +76,7 @@ export function Input(props) {
   })(error)
 
   return (
-    <input
-      style={finalStyle}
+    <StyledInput
       { ...filterHTMLPropsOnly }
     />
     )
